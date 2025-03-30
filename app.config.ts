2771d4011b3040435b2e8fb4896bb81eacdbe310
James Karlsson <jameskarlsson1@gmail.com>
2025-03-30 17:20:31 +1100

    Remove async hooks

--- app.config.ts
+++ app.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     preset: "cloudflare-pages",
     rollupConfig: {
-      external: ["node:async_hooks"],
+      //  external: ["node:async_hooks"],
     },
   },
 });
