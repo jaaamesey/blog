a8af3568af7acce071945d9dba4af26f1b533816
James Karlsson <jameskarlsson1@gmail.com>
2025-05-19 00:03:14 +1000

    Better home page

--- src/routes/(index)/index.tsx
+++ src/routes/(index)/index.tsx
@@ -1,38 +1,37 @@
 import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
 import { allPosts } from "~/all_posts.compile";
 
 export default function Home() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      test
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
+    <main class="text-center mx-auto p-4 flex flex-col items-center gap-2">
+      <h1 class="max-6-xs text-2xl text-sky-700 font-thin my-16">
+        bikeshedd.ing:
+        <br />
+        james karlsson's blog/page/whatever
       </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
-      {allPosts?.map((p) => (
-        <div>
-          <A href={`/posts/${p.id}`}>{JSON.stringify(p)}</A>
-        </div>
-      ))}
+      <div class="flex gap-4 mb-10">
+        <a href="https://github.com/jaaamesey">github</a>
+        <a href="https://www.canva.com/">work</a>
+        <a href="https://www.linkedin.com/in/james-karlsson-43304314b/">
+          linkedin
+        </a>
+      </div>
+      <h2>Posts</h2>
+      <div class="flex w-96 justify-end">
+        {allPosts?.map((p) => (
+          <A
+            class="w-full bg-white rounded-2xl p-4 text-start text-gray-950"
+            href={`/posts/${p.id}`}
+          >
+            <div class="flex justify-between">
+              <div>
+                <em>{p.date}</em>
+              </div>
+              <div>{p.title}</div>
+            </div>
+          </A>
+        ))}
+      </div>
     </main>
   );
 }
