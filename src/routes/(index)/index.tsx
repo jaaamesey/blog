f046d395726c62014ec06af0a908afcf898d054e
James Karlsson <jameskarlsson1@gmail.com>
2025-05-19 00:08:11 +1000

    Cleanup

--- src/routes/(index)/index.tsx
+++ src/routes/(index)/index.tsx
@@ -4,19 +4,18 @@ import { allPosts } from "~/all_posts.compile";
 export default function Home() {
   return (
     <main class="text-center mx-auto p-4 flex flex-col items-center gap-2">
-      <h1 class="max-6-xs text-2xl text-sky-700 font-thin my-16">
-        bikeshedd.ing:
-        <br />
-        james karlsson's blog/page/whatever
+      <h1 class="max-6-xs text-2xl text-sky-700 font-thin my-6">
+        james karlsson
       </h1>
-      <div class="flex gap-4 mb-10">
+      <div class="mb-6 text-sm">i makea da software</div>
+      <div class="flex gap-4 mb-6">
         <a href="https://github.com/jaaamesey">github</a>
         <a href="https://www.canva.com/">work</a>
         <a href="https://www.linkedin.com/in/james-karlsson-43304314b/">
           linkedin
         </a>
       </div>
-      <h2>Posts</h2>
+      <h2 class="font-bold">posts</h2>
       <div class="flex w-96 justify-end">
         {allPosts?.map((p) => (
           <A
