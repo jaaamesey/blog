9bdd6a08696a296a7c1ed429caf22e6563934b25
James Karlsson <jameskarlsson1@gmail.com>
2025-05-18 18:52:32 +1000

    Props work

--- src/routes/posts/how_this_blog_works/some-react-component.react.tsx
+++ src/routes/posts/how_this_blog_works/some-react-component.react.tsx
@@ -1,11 +1,8 @@
-// TODO: Add this automatically somehow?
 import * as React from "react";
-import { createElement, memo, useState } from "react";
-import { createRoot } from "react-dom/client";
-import { reactToSolid } from "~/utils/embed_react_component";
+import { memo, useState } from "react";
 
-export const SomeReactComponent = memo(() => {
-  const [count, setCount] = useState(0);
+export const SomeReactComponent = memo((props: { start: number }) => {
+  const [count, setCount] = useState(props.start);
   return (
     <button onClick={() => setCount((n) => n + 1)}>{count}</button>
   ) as React.ReactNode;
