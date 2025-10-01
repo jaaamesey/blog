8c4663c36946e7ff4e48fa2a790a2efe89445047
James Karlsson <james.karlsson@canva.com>
2025-10-02 02:43:00 +1000

    Cleanup

--- src/routes/posts/_how_much_of_that_cloudflare_outage_was_useeffects_fault/meta.ts
+++ src/routes/posts/_how_much_of_that_cloudflare_outage_was_useeffects_fault/meta.ts
@@ -1,7 +1,7 @@
 import { PostConfig } from "~/post_types";
 
 export default {
-  title: "How much of that Cloudflare outage was actually React's fault?",
-  date: "2025-10-01",
+  title: "How much of that Cloudflare outage was actually useEffect's fault?",
+  date: "2025-10-02",
   description: 'I can\'t believe I\'m actually going to defend useEffect.'
 } satisfies PostConfig;
