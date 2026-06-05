027303159fbfc7bcfe96bdff734eda1f1557473d
James Karlsson <james.karlsson@canva.com>
2026-06-05 16:13:09 +1000

    Add tsx.zone article

--- src/routes/posts/we-have-codesandbox-at-home/meta.ts
+++ src/routes/posts/we-have-codesandbox-at-home/meta.ts
@@ -2,6 +2,6 @@ import type { PostConfig } from "~/post_types";
 
 export default {
   title: '"We have CodeSandbox at home"',
-  date: "2025-10-24",
+  date: "2026-06-05",
   description: ''
 } satisfies PostConfig;
--- src/utils/test_counter.react.tsx
+++ src/utils/test_counter.react.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react'
 import { useState } from "react";
 
-export function CounterReact() {
+export function CounterReact(props: {children: React.ReactNode}) {
     const [c, setC] = useState(0)
-    return <button onClick={() => setC(c=> c+1)}>{c}</button>
+    return <><button onClick={() => setC(c=> c+1)}>{c}</button><div style={{background: 'grey'}}>{props.children}</div></>
 }
